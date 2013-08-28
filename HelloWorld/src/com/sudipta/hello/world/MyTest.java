1b62a2b883335a742a91dc14dd225b2afe4e66b6
Sudipta Deb <sudipta.deb@gmail.com>
2013-08-28 23:17:13 +0530

    Initial commit for the Hello World Project

---
+++ HelloWorld/src/com/sudipta/hello/world/MyTest.java
@@ -0,0 +1,14 @@
+package com.sudipta.hello.world;
+
+public class MyTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello to GIT World");
+		System.out.println("Added the new line for the first commit");
+
+	}
+
+}
