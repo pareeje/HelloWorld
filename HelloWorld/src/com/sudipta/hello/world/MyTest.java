c44d1b2cd95cb17fb059c2dbdfdd5dce650a0a28
Sudipta Deb <sudipta.deb@gmail.com>
2013-08-28 23:25:10 +0530

    Added another print message to check the first change

--- HelloWorld/src/com/sudipta/hello/world/MyTest.java
+++ HelloWorld/src/com/sudipta/hello/world/MyTest.java
@@ -8,6 +8,7 @@ public class MyTest {
 	public static void main(String[] args) {
 		System.out.println("Hello to GIT World");
 		System.out.println("Added the new line for the first commit");
+		System.out.println("Let this be my first change");
 
 	}
 
